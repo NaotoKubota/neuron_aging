f4ca09919d03d082df0a0ef8f2d1925f30e6d276
NaotoKubota <the.owner.of.the.dream123@gmail.com>
2026-07-28 21:08:27 -0700

    QC and mapping for GSE233509 and GSE249105

---
+++ scripts/qc_mapping/qc_mapping_GSE249105.bash
@@ -0,0 +1,38 @@
+#!/bin/bash -l
+
+#####################################################################
+
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=64
+#SBATCH --mem=80G
+#SBATCH --mail-user=the.owner.of.the.dream123@gmail.com
+#SBATCH --mail-type=ALL
+#SBATCH --job-name="QC mapping GSE249105"
+#SBATCH -p epyc
+
+# Print current date
+date
+
+#####################################################################
+
+
+# load modules
+module load singularity snakemake
+
+
+# Run preprocessing_RNAseq.smk
+snakemake -s /rhome/naotok/bigdata/SnakeNgs/snakefile/preprocessing_RNAseq.smk \
+--configfile config_GSE249105.yaml \
+--cores 64 \
+--use-singularity \
+--singularity-args "--bind $HOME:$HOME" \
+--rerun-incomplete
+
+
+# Print end date
+date
+
+
+# Print name of node
+hostname
